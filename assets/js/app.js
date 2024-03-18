faae37761e07eb9e89146e4faf366c00d2b006fb
Rohit-Dhaka <rohitdhaka2110@gamil.com>
2024-03-18 14:38:04 +0530

    CountDown Time add

--- assets/js/app.js
+++ assets/js/app.js
@@ -151,3 +151,21 @@ decrement.onclick  = function() {
   count -= 1;
   counts.innerHTML = count;
 }
+
+
+// -----------CountDown Time
+var countDownDate = new Date("Apr 27, 2024 18:37:25").getTime();
+var x = setInterval(function () {  
+  var now = new Date().getTime();  
+  var distance = countDownDate - now;  
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);  
+  document.getElementById("demo").innerHTML = days + ":" + hours + ":"
+    + minutes + ":" + seconds;  
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
