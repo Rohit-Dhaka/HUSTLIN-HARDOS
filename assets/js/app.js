0225efc72a04c838d88ef29d3687c1cde1293948
Rohit-Dhaka <rohitdhaka2110@gamil.com>
2024-03-18 13:46:19 +0530

    add first slider

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+// ---------First
+$('.first').slick({
+    infinite: true,
+    dots: false,
+    autoplay: true,
+    variableWidth: true,
+    speed: 300,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplaySpeed: 0,
+    speed: 2000,
+    pauseOnHover: false,
+    cssEase: 'linear',  
+  });
