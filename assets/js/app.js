11fa5dd18b68e9c1707d219c59cdac21f98a5bd7
Rohit-Dhaka <rohitdhaka2110@gamil.com>
2024-03-18 14:11:58 +0530

    2nd slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,3 +30,40 @@ function mobileMenu() {
   document.body.classList.toggle("overflow-hidden");
   navMenu.classList.toggle("nav-active");
 }
+
+
+
+// --------Tweet-Slider
+$('.tweet-slider').slick({
+    prevArrow: ".pre-btn",
+    nextArrow: ".next-btn",
+    infinite: true,
+    dots: false,  
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,                        
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,        
+        }
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,        
+          cssEase: 'linear'
+        }
+      }
+    ]
+  });
