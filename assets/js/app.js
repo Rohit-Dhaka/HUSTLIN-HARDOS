23ef1217eb83db68c205ae2cdf0a6f1b551532f9
Rohit-Dhaka <rohitdhaka2110@gamil.com>
2024-03-18 14:36:40 +0530

    go to top button{

--- assets/js/app.js
+++ assets/js/app.js
@@ -122,3 +122,19 @@ accordion.forEach((e) => {
 var d = new Date();
 let year = d.getFullYear();
 document.getElementById("year").innerHTML = year
+
+
+//------go to top
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
